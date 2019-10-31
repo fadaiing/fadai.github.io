7fba7b394460b2bcb19722eba89907fb6d65456a
lfanc <44469925+lfanc@users.noreply.github.com>
2019-10-31 19:18:11 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = '发呆ing～～～' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = '发呆ing～～～' + storedName;
+}
+myButton.onclick = function() {
+   setUserName();
+}
